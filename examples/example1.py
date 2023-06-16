8a13ad0cef1dd4fb0181f5ce44a0b49e44cdbfb2
Aoibheann1 <alilymurray247@gmail.com>
2023-06-16 22:14:37 +0100

    updates

--- examples/example1.py
+++ examples/example1.py
@@ -7,10 +7,10 @@ a = 1e5
 x_start = -1e-1
 x_end = 1e-3
 t_start = 0.0
-t_end = 1e6
+t_end = 1.0
 n_x = 1000
 
 a1 = d1 / d2 * (x_end / x_start) ** 2
 a2 = d1 / d2 * abs(x_end / x_start) / a
 # Solve and visualize the diffusion equation
-solve(a1, a2, x_start, x_end, t_start, t_end, n_x)
+solve(a1, a2, -1, 1, t_start, t_end, n_x)
